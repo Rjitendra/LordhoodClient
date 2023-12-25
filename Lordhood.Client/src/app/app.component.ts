b93ea0d704f9e0e5640622ed4c8e7071ae915bdc
Jitendra Behera <jb.jitu@live.com>
2023-12-25 14:19:53 +0530

    changes

--- Lordhood.Client/src/app/app.component.ts
+++ Lordhood.Client/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'app-root',
