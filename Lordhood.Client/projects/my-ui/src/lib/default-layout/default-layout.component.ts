b64599c5e911ef2ad621e9b3c1d04db689cff25b
Jitendra Behera <jb.jitu@live.com>
2023-12-25 21:13:06 +0530

    changes

---
+++ Lordhood.Client/projects/my-ui/src/lib/default-layout/default-layout.component.ts
@@ -0,0 +1,79 @@
+import { Component, OnInit } from '@angular/core';
+
+import { navItems } from './_nav';
+import { Title } from '@angular/platform-browser';
+import { IconSetService, IconModule } from '@coreui/icons-angular';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { DefaultFooterComponent } from './default-footer/default-footer.component';
+import { DefaultHeaderComponent } from './default-header/default-header.component';
+import { NgScrollbarModule } from 'ngx-scrollbar';
+import {
+  SidebarComponent,
+  SidebarBrandComponent,
+  SidebarNavComponent,
+  SidebarTogglerComponent,
+  SidebarToggleDirective,
+  ContainerComponent,
+} from '@coreui/angular';
+import { iconSubset } from '../icons/icon-subset';
+
+import {
+  AvatarModule,
+  FooterModule,
+  DropdownModule,
+  GridModule,
+  HeaderModule,
+  SidebarModule,
+  NavModule,
+  BadgeModule,
+} from '@coreui/angular';
+@Component({
+  selector: 'app-core-ui',
+  templateUrl: './default-layout.component.html',
+  standalone: true,
+  imports: [
+    AvatarModule,
+    FooterModule,
+    DropdownModule,
+    GridModule,
+    HeaderModule,
+    SidebarModule,
+    IconModule,
+    NavModule,
+    BadgeModule,
+    // SidebarComponent,
+    // SidebarBrandComponent,
+  //  RouterLink,
+    NgScrollbarModule,
+    // SidebarNavComponent,
+    // SidebarTogglerComponent,
+    //SidebarToggleDirective,
+    DefaultHeaderComponent,
+  //  ContainerComponent,
+   // RouterOutlet,
+    DefaultFooterComponent,
+  ],
+  providers: [IconSetService],
+})
+export class DefaultLayoutComponent implements OnInit {
+  loading!: boolean;
+  public navItems = navItems;
+  title = 'Lordhood';
+  public perfectScrollbarConfig = {
+    suppressScrollX: true,
+  };
+
+  constructor(
+    private titleService: Title,
+    private iconSetService: IconSetService,
+
+    private router: Router
+  ) {
+    this.titleService.setTitle(this.title);
+    this.iconSetService.icons = { ...iconSubset };
+  }
+
+  ngOnInit() {}
+
+  openModal() {}
+}
---
+++ Lordhood.Client/projects/my-ui/src/lib/my-ui.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-my-ui',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      my-ui works!
+    </p>
+  `,
+  styles: ``
+})
+export class MyUiComponent {
+
+}
