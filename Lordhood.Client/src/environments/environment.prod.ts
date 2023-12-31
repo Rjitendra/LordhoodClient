ccdf524f3299a0a476812be4a380b40db9af1b12
Jitendra Behera <jb.jitu@live.com>
2023-12-31 18:18:01 +0530

    ch

--- Lordhood.Client/src/environments/environment.prod.ts
+++ Lordhood.Client/src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
     clientBaseUrl: 'https://lordhood.me',
     apiBaseUrl: 'https://lordhood.me/Api/api/',
     stsBaseUrl: 'https://lordhood.me/STS',
-    stripe_api_key: 'pk_live_51MvHYRIK3Ulf3wYeZOfJM87YtyFkvitmCYHqITd2NVdlsgFMyWWYBWfuJ65xFp2HDnps7UpAfw9FHeia6OhjEJ6Y00jdoaoqZ2',
+    stripe_api_key: '',
     addressKey: '-ljoqanEokmGQGzEhFJVzQ39085'
   };
